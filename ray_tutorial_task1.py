8fc02d100ed686fc0c8f1fa4f7547cc87b16586f
Kavinda Senewiratne <kavinda.ais@gmail.com>
2021-04-05 17:22:01 -0700

    task3 attempt 1.

--- ray_tutorial_task1.py
+++ ray_tutorial_task1.py
@@ -39,8 +39,66 @@ def task2():
     assert duration > 1, ('The loop took {} seconds. This is too fast.'
                       .format(duration)) 
 
+    print('Success! The example took {} seconds.'.format(duration))
+
 
 #TASK 3
 
+def task3():
+
+    @ray.remote
+    def load_data(filename):
+        time.sleep(0.1)
+        return np.ones((1000, 100))
+
+    @ray.remote
+    def normalize_data(data):
+        time.sleep(0.1)
+        return data - np.mean(data, axis=0)
+
+    @ray.remote
+    def extract_features(normalized_data):
+        time.sleep(0.1)
+        return np.hstack([normalized_data, normalized_data ** 2])
+
+    @ray.remote
+    def compute_loss(features):
+        num_data, dim = features.shape
+        time.sleep(0.1)
+        return np.sum((np.dot(features, np.ones(dim)) - np.ones(num_data)) ** 2)
+
+    assert hasattr(load_data, 'remote'), 'load_data must be a remote function'
+    assert hasattr(normalize_data, 'remote'), 'normalize_data must be a remote function'
+    assert hasattr(extract_features, 'remote'), 'extract_features must be a remote function'
+    assert hasattr(compute_loss, 'remote'), 'compute_loss must be a remote function'
+
+    time.sleep(2.0)
+    start_time = time.time()
+
+    losses = []
+    for filename in ['file1', 'file2', 'file3', 'file4']:
+        inner_start = time.time()
+
+        data = load_data.remote(filename)
+        normalized_data = normalize_data.remote(data)
+        features = extract_features.remote(normalized_data)
+        loss = compute_loss.remote(features)
+        losses.append(loss)
+        
+        inner_end = time.time()
+        
+        if inner_end - inner_start >= 0.1:
+            raise Exception('You may be calling ray.get inside of the for loop! '
+                            'Doing this will prevent parallelism from being exposed. '
+                            'Make sure to only call ray.get once outside of the for loop.')
+
+    print('The losses are {}.'.format(losses) + '\n')
+    loss = sum(ray.get(losses))
+
+    end_time = time.time()
+    duration = end_time - start_time
+
+    print('The loss is {}. This took {} seconds. Run the next cell to see '
+        'if the exercise was done correctly.'.format(loss, duration))
 
 
