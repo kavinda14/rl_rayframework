39ba7037843258ff622c4726804cb4ca9cb9311b
Kavinda Senewiratne <kavinda.ais@gmail.com>
2021-04-09 10:34:34 -0700

    3 cpu

--- map_reduce-2.py
+++ map_reduce-2.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
-        cpus = 4
+        cpus = 3
     else:
         cpus = int(sys.argv[1])
     print('running map reduce with %d cpus' % cpus)
